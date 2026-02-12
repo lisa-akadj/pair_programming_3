d0b3cb656e9a1de00b60082dc41b1029531c89db
davidkapcsandi <david_kapcsandi@hotmail.com>
2026-02-12 17:11:31 +0000

    added updated

--- lib/playlist.py
+++ lib/playlist.py
@@ -1,15 +1,14 @@
 class Playlist():
    
-    def __init__(self):
-     self.tracks = []
+   def __init__(self):
+      self.tracks = []
 
-    def add(self, song):
-       self.song = song 
-       self.tracks.append(song)
-       
+   def add(self, song):
+      self.song = song 
+      self.tracks.append(song)
 
-    def list_of_tracks(self):
-       return self.tracks
-         
-      
-    
+
+   def list_of_tracks(self):
+      if self.tracks == []:
+         raise TypeError("list is empty")
+      return list(self.tracks)
--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,6 +1,14 @@
+import pytest
 from lib.playlist import *
 
 def test_add_song():
     playlist = Playlist()
     playlist.add("Umbrella by Rihanna")
     assert playlist.tracks == ['Umbrella by Rihanna']
+
+def test_typerror_in_list_empty():
+    playlist = Playlist()
+    with pytest.raises(TypeError):
+        playlist.add()
+    assert playlist.list_of_tracks == "Playlist is empty"
+        
